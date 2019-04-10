09c3d33370efa32c612f81d9bd95ec21405178b2
MaximUzun <uzun_maksim@yahoo.com>
2019-04-10 12:12:31 +0300

    HomeWork Done

--- home_work_basic_L2/hw02_easy.py
+++ home_work_basic_L2/hw02_easy.py
@@ -1,3 +1,4 @@
+# Автор - Узун Максим Владимирович
 # Задача-1:
 # Дан список фруктов.
 # Напишите программу, выводящую фрукты в виде нумерованного списка,
@@ -12,14 +13,40 @@
 # 4.  арбуз
 
 # Подсказка: воспользоваться методом .format()
-
+print('Задача 1')
+fruit_list = ["яблоко", "банан", "киви", "арбуз"]
+for index, value in enumerate(fruit_list, 1):
+    print("{}.{:>10}".format(index, value))
 
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
+print('   ')
+print('Задача 2')
+numbers_list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers_list2 = [1, 3, 5, 7, 9]
 
+print(numbers_list1)
+print(numbers_list2)
+
+for elements in numbers_list2:
+    if elements in numbers_list1:
+            numbers_list1.remove(elements)
+print(numbers_list1)
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
 # Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
 # если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+print('   ')
+print('Задача 3')
+number_list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+number_list2 = []
+print(number_list1)
+
+for digit in number_list1:
+    if digit % 2 == 0:
+        number_list2.append(digit / 4)
+    else:
+        number_list2.append(digit * 2)
+print(number_list2)
--- home_work_basic_L2/hw02_hard.py
+++ home_work_basic_L2/hw02_hard.py
@@ -1,10 +1,21 @@
+# Автор - Узун Максим Владимирович
 # Задание-1: уравнение прямой вида y = kx + b задано в виде строки.
 # Определить координату y точки с заданной координатой x.
-
 equation = 'y = -12x + 11111140.2121'
 x = 2.5
 # вычислите и выведите y
+print('Задача 1')
+eq = equation.split()
+print(eq)
+x = float(x)
+y = 0
+tmp_var = float(eq[2][:-1])
+if eq[3] == '+':
+    y = tmp_var*x + float(eq[4])
+else:
+    y = tmp_var*x - float(eq[4])
 
+print(y)
 
 # Задание-2: Дата задана в виде строки формата 'dd.mm.yyyy'.
 # Проверить, корректно ли введена дата.
@@ -24,6 +35,25 @@ date = '01.22.1001'
 date = '1.12.1001'
 date = '-2.10.3001'
 
+print('   ')
+print('Задача 2')
+
+date_format = date.split('.')
+day = date_format[0]
+month = date_format[1]
+year = date_format[2]
+month_31 = ['01', '03', '05', '07', '08', '10', '12']
+month_30 = ['02', '04', '06', '09', '11']
+
+if len(day) == 2 and len(month) == 2 and len(year) == 4:
+    if month in month_31 and 1 <= int(day) <= 31 and 1 <= int(year) <= 9999:
+        print('Дата введена корректно')
+    elif month in month_30 and 1 <= int(day) <= 30 and 1 <= int(year) <= 9999:
+        print('Дата введена корректно')
+    else:
+        print('Дата некорректна')
+else:
+    print('Дата некорректна')
 
 # Задание-3: "Перевёрнутая башня" (Задача олимпиадного уровня)
 #
@@ -54,3 +84,54 @@ date = '-2.10.3001'
 #
 # Вход: 11
 # Выход: 5 3
+print('   ')
+print('Задача 3')
+
+N = 10
+
+cell_list = [ i*i for i in range(1, N)]
+lvl_list = [i for i in range(1, N)]
+print(cell_list)
+print(lvl_list)
+
+room = 13
+number = 0
+
+count = 0
+for cell in cell_list:
+    number += cell
+    count += 1
+    if number >= room:
+        break
+
+print('Номер блока, где нужная комната, количество этажей в блоке и количество комнат на этаже: ' + str(count))
+
+slice_cell_list = cell_list[:count]
+print(slice_cell_list)
+
+last_room = sum(slice_cell_list)
+print('Последняя комната в нужном нам блоке имеет номер: ' + str(last_room))
+
+first_room = last_room - slice_cell_list[-1] + 1
+print('Первая комната в  блоке имеет номер: ' + str(first_room))
+
+before_lvl_number = sum(lvl_list[:(count - 1)])
+print('Этажей до блока: ' + str(before_lvl_number))
+
+inner_lvl = 1
+end = first_room + count
+
+for j in range(count):
+    just_list = range(first_room, end)
+    if room in just_list:
+        inner_room_place = just_list.index(room) +1
+        print('Положение комнаты на этаже: ' + str(inner_room_place))
+        break
+    first_room += count
+    end += count
+    inner_lvl += 1
+final_lvl = inner_lvl + before_lvl_number
+print('Комната находится на этаже: ' + str(final_lvl))
+
+print('Вход: ', room)
+print('Выход: ', str(final_lvl), str(inner_room_place))
