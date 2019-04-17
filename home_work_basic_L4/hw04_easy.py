38a0ddc55e34ec3ce667f3c699a01920deefa44e
MaximUzun <uzun_maksim@yahoo.com>
2019-04-17 10:12:47 +0300

    Home work done

--- home_work_basic_L4/hw04_easy.py
+++ home_work_basic_L4/hw04_easy.py
@@ -1,3 +1,4 @@
+# Автор Узун М.В.
 # Все задачи текущего блока решите с помощью генераторов списков!
 
 # Задание-1:
@@ -6,9 +7,24 @@
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+import random
+
+origin_list = [random.randint(1, 10) for i in range(7)]
+
+edit_list = [i ** 2 for i in origin_list]
+
+input("Press Enter")
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+list1 = ['apple', 'banana', 'cherry', 'fig', 'grape', 'honeydrew', 'lime']
+list2 = ['mango', 'peach', 'quince', 'cherry', 'lemon', 'apple', 'fig', 'tomato']
+
+new_list = [i for i in list1 if i in list2]
+
+input("Press Enter")
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +32,11 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+import random
+
+origin_list = [random.randint(-10, 30) for i in range(15)]
+
+edit_list = [i for i in origin_list if i % 3 == 0 and i > 0 and i % 4 != 0]
+
+input("Press Enter")
--- home_work_basic_L4/hw04_normal.py
+++ home_work_basic_L4/hw04_normal.py
@@ -1,3 +1,4 @@
+# Автор Узун М.В.
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
@@ -20,6 +21,35 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+# 're'
+
+import re
+
+result = re.findall(r'[^A-Z]+', line)
+
+print(result)
+
+input("Press Enter")
+
+# без 're'
+
+result = ''
+
+for i in line:
+    if i.islower():
+        result += i
+    else:
+        result += '_'
+
+result = result.split('_')
+
+for i in result[:]:
+    if i == '':
+        result.remove(i)
+
+print(result)
+
+input("Press Enter")
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +75,88 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+# 're'
+
+import re
+
+result = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+
+print(result)
+
+input("Press Enter")
+
+# без 're' но с google.com
+
+line_coded = ''
+
+for i in line_2:
+    if i.islower():
+        line_coded += '.'
+    else:
+        line_coded += '!'
+
+slices = []
+
+for i in range(len(line_coded)):
+    temp_str = ''
+    if i < len(line_coded) - 2:
+        if line_coded[i] == '.' and line_coded[i+1] == '.' and line_coded[i+2] == '!':
+            temp_str += line_coded[i] + line_coded[i+1] + line_coded[i+2]
+            for x in range(i+2, len(line_coded)):
+                if line_coded[x] == '.':
+                    break
+                else:
+                    temp_str += line_coded[x]
+            if len(temp_str) >= 6:
+                slices.append([i, x])
+result = []
+
+for i in slices:
+    result.append(line_2[i[0]+2:i[1]-2])
+
+print(result)
+
+input("Press Enter")
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+import re
+
+digit_line = str(random.randint(1, 9))
+
+for i in range(2499):
+    digit_line += str(random.randint(0, 9))
+
+with open('output_data.txt', 'w') as output_data:
+    output_data.write(digit_line)
+
+with open('output_data.txt', 'r') as read_data:
+    read_line = read_data.read()
+
+alt_pattern = re.compile(r'(\d)\1{2,}')
+
+pattern = re.compile(r'0{2,}|1{2,}|2{2,}|3{2,}|4{2,}|5{2,}|6{2,}|7{2,}|7{2,}|8{2,}|9{2,}')
+
+result = pattern.findall(read_line)
+
+max_len = 0
+
+for i in result:
+    if len(i) > max_len:
+        max_len = len(i)
+
+end_result = []
+
+for i in result:
+    if len(i) == max_len:
+        end_result.append(i)
+
+print(end_result)
+
+input("Press Enter")
